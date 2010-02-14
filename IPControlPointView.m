5c1bb32a668e7c27411b9432cc8d0b7ad4550c54
Tim Horton <hortont424@gmail.com>
2010-02-14 15:59:46 -0500

    Add symmetric sample point

--- IPControlPointView.m
+++ IPControlPointView.m
@@ -26,6 +26,24 @@
         
         [controlPoints addObject:pt];
         [self createTrackingAreasForControlPoint:pt];
+        
+        pt = [[IPControlPoint alloc] init];
+        [pt setPoint:NSMakePoint(150, 150)];
+        [pt setType:IP_CONTROL_POINT_SMOOTH];
+        [pt setControlPoint:0 toPoint:NSMakePoint(30, 15)];
+        [pt setControlPoint:1 toPoint:NSMakePoint(0, -12)];
+        
+        [controlPoints addObject:pt];
+        [self createTrackingAreasForControlPoint:pt];
+        
+        pt = [[IPControlPoint alloc] init];
+        [pt setPoint:NSMakePoint(300, 200)];
+        [pt setType:IP_CONTROL_POINT_SMOOTH_SYMMETRIC];
+        [pt setControlPoint:0 toPoint:NSMakePoint(30, 30)];
+        [pt setControlPoint:1 toPoint:NSMakePoint(-30, -30)];
+        
+        [controlPoints addObject:pt];
+        [self createTrackingAreasForControlPoint:pt];
     }
     return self;
 }
