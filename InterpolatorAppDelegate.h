e67cc12f6c8ef03b676147a5737a6b07bfc3674e
Tim Horton <hortont424@gmail.com>
2010-02-14 00:18:37 -0500

    Initial template-style fixes, remove stock comments, etc.

--- InterpolatorAppDelegate.h
+++ InterpolatorAppDelegate.h
@@ -1,14 +1,7 @@
-//
-//  InterpolatorAppDelegate.h
-//  Interpolator
-//
-//  Created by Timothy Horton on 2010.02.13.
-//  Copyright 2010 Rensselaer Polytechnic Institute. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
-@interface InterpolatorAppDelegate : NSObject <NSApplicationDelegate> {
+@interface InterpolatorAppDelegate : NSObject <NSApplicationDelegate>
+{
     NSWindow *window;
 }
 
