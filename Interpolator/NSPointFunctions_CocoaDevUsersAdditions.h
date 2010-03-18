99627eda4e86b0555020100fbd18d1702dafd001
Tim Horton <hortont424@gmail.com>
2010-03-17 23:47:04 -0400

    Reduce amount of crap in NSPointAdditions

--- Interpolator/NSPointFunctions_CocoaDevUsersAdditions.h
+++ Interpolator/NSPointFunctions_CocoaDevUsersAdditions.h
@@ -5,9 +5,8 @@
 
 #import <Cocoa/Cocoa.h>
 #import <math.h>
-#import <float.h> /* Standard C lib that contains some constants, look at http://www.acm.uiuc.edu/webmonkeys/book/c_guide/2.4.html -- JP */
 
-extern const NSPoint NSFarAwayPoint; // FLT_MAX = 1E+27.  -- JP
+extern const NSPoint NSFarAwayPoint;
 
 static inline NSPoint NSAddPoints(NSPoint firstPoint, NSPoint secondPoint)
 {
@@ -19,39 +18,17 @@ static inline NSPoint NSSubtractPoints(NSPoint firstPoint, NSPoint secondPoint)
 	return NSMakePoint(firstPoint.x-secondPoint.x, firstPoint.y-secondPoint.y);
 }
 
-static inline NSPoint NSOffsetPoint(NSPoint point, float amountX, float amountY)
-{
-    return NSAddPoints(point, NSMakePoint(amountX, amountY));
-}
-
-static inline NSPoint NSReflectedPointAboutXAxis(NSPoint point)
-{
-    return NSMakePoint(-point.x, point.y);
-}
-
-static inline NSPoint NSReflectedPointAboutYAxis(NSPoint point)
-{
-    return NSMakePoint(point.x, -point.y);
-}
-
-static inline NSPoint NSReflectedPointAboutOrigin(NSPoint point)
-{
-    return NSMakePoint(-point.x, -point.y);
-}
-
-static inline NSPoint NSTransformedPoint(NSPoint point, NSAffineTransform *transform)
-{
-	return [transform transformPoint:point];
-}
-
 static inline NSPoint NSCartesianToPolar(NSPoint cartesianPoint)
 {
-    return NSMakePoint(sqrtf(cartesianPoint.x*cartesianPoint.x+cartesianPoint.y*cartesianPoint.y), atan2f(cartesianPoint.y,cartesianPoint.x));
+    return NSMakePoint(sqrtf(cartesianPoint.x * cartesianPoint.x +
+                             cartesianPoint.y * cartesianPoint.y),
+                       atan2f(cartesianPoint.y, cartesianPoint.x));
 }
 
 static inline NSPoint NSPolarToCartesian(NSPoint polarPoint)
 {
-    return NSMakePoint(polarPoint.x*cosf(polarPoint.y), polarPoint.x*sinf(polarPoint.y));
+    return NSMakePoint(polarPoint.x * cosf(polarPoint.y),
+                       polarPoint.x * sinf(polarPoint.y));
 }
 
 // Everything below is covered under toplevel license and written by me
--- Interpolator/NSPointFunctions_CocoaDevUsersAdditions.m
+++ Interpolator/NSPointFunctions_CocoaDevUsersAdditions.m
@@ -1,3 +1,4 @@
 #import "NSPointFunctions_CocoaDevUsersAdditions.h"
+#import <float.h>
 
 const NSPoint NSFarAwayPoint = {FLT_MAX, FLT_MAX};
