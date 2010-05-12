c47ce88ca31a46aef343fccf26daba20755b7106
Tim Horton <hortont424@gmail.com>
2010-05-12 02:45:15 -0400

    Stuff

--- Simulator/main.c
+++ Simulator/main.c
@@ -50,7 +50,7 @@ int main(int argc, const char ** argv)
 {
     SMOptionsParse(argc, argv);
 
-    simulator = SMSimulatorNewFromFile("../Systems/sample.psys");
+    simulator = SMSimulatorNewFromFile("../Systems/n-body.psys");
     SMSimulatorPushData(simulator);
 
     if(simulatorOutputMode == SM_PREVIEWER_OUTPUT)
